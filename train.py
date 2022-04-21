de37b4e69a524c4354fc14e9c59b41841762e967
wade0125 <100984053+wade0125@users.noreply.github.com>
2022-04-21 11:17:31 +0800

    2022/04/21

---
+++ train.py
@@ -0,0 +1,250 @@
+from functools import partial
+
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=10240)])
+  except RuntimeError as e:
+    print(e)
+import tensorflow.keras.backend as K
+from tensorflow.keras.optimizers import Adam
+
+from nets.frcnn import get_model
+from nets.frcnn_training import (ProposalTargetCreator, classifier_cls_loss,
+                                 classifier_smooth_l1, rpn_cls_loss,
+                                 rpn_smooth_l1)
+from utils.anchors import get_anchors
+from utils.callbacks import LossHistory
+from utils.dataloader import FRCNNDatasets
+from utils.utils import get_classes
+from utils.utils_bbox import BBoxUtility
+from utils.utils_fit import fit_one_epoch
+
+    
+'''
+訓練自己的目標檢測模型一定需要注意以下幾點：
+1、訓練前仔細檢查自己的格式是否滿足要求，該庫要求數據集格式為VOC格式，需要準備好的內容有輸入圖片和標籤
+   輸入圖片為.jpg圖片，無需固定大小，傳入訓練前會自動進行resize。
+   灰度圖會自動轉成RGB圖片進行訓練，無需自己修改。
+   輸入圖片如果後綴非jpg，需要自己批量轉成jpg後再開始訓練。
+
+   標籤為.xml格式，文件中會有需要檢測的目標信息，標籤文件和輸入圖片文件相對應。
+
+2、訓練好的權值文件保存在logs文件夾中，每個epoch都會保存一次，如果只是訓練了幾個step是不會保存的，epoch和step的概念要捋清楚一下。
+   在訓練過程中，該代碼並沒有設定只保存最低損失的，因此按默認參數訓練完會有100個權值，如果空間不夠可以自行刪除。
+   這個並不是保存越少越好也不是保存越多越好，有人想要都保存、有人想只保存一點，為了滿足大多數的需求，還是都保存可選擇性高。
+
+3、損失值的大小用於判斷是否收斂，比較重要的是有收斂的趨勢，即驗證集損失不斷下降，如果驗證集損失基本上不改變的話，模型基本上就收斂了。
+   損失值的具體大小並沒有什麼意義，大和小只在於損失的計算方式，並不是接近於0才好。如果想要讓損失好看點，可以直接到對應的損失函數里面除上10000。
+   訓練過程中的損失值會保存在logs文件夾下的loss_%Y_%m_%d_%H_%M_%S文件夾中
+
+4、調參是一門蠻重要的學問，沒有什麼參數是一定好的，現有的參數是我測試過可以正常訓練的參數，因此我會建議用現有的參數。
+   但是參數本身並不是絕對的，比如隨著batch的增大學習率也可以增大，效果也會好一些；過深的網絡不要用太大的學習率等等。
+   這些都是經驗上，只能靠各位同學多查詢資料和自己試試了。
+'''  
+if __name__ == "__main__":
+    #--------------------------------------------------------#
+    #   训练前一定要修改classes_path，使其对应自己的数据集
+    #--------------------------------------------------------#
+    classes_path    = 'model_data/voc_classes.txt'
+    #----------------------------------------------------------------------------------------------------------------------------#
+    #   权值文件的下载请看README，可以通过网盘下载。模型的 预训练权重 对不同数据集是通用的，因为特征是通用的。
+    #   模型的 预训练权重 比较重要的部分是 主干特征提取网络的权值部分，用于进行特征提取。
+    #   预训练权重对于99%的情况都必须要用，不用的话主干部分的权值太过随机，特征提取效果不明显，网络训练的结果也不会好
+    #
+    #   如果训练过程中存在中断训练的操作，可以将model_path设置成logs文件夹下的权值文件，将已经训练了一部分的权值再次载入。
+    #   同时修改下方的 冻结阶段 或者 解冻阶段 的参数，来保证模型epoch的连续性。
+    #   
+    #   当model_path = ''的时候不加载整个模型的权值。
+    #
+    #   此处使用的是整个模型的权重，因此是在train.py进行加载的。
+    #   如果想要让模型从主干的预训练权值开始训练，则设置model_path为主干网络的权值，此时仅加载主干。
+    #   如果想要让模型从0开始训练，则设置model_path = ''，Freeze_Train = Fasle，此时从0开始训练，且没有冻结主干的过程。
+    #   一般来讲，从0开始训练效果会很差，因为权值太过随机，特征提取效果不明显。
+    #
+    #   网络一般不从0开始训练，至少会使用主干部分的权值，有些论文提到可以不用预训练，主要原因是他们 数据集较大 且 调参能力优秀。
+    #   如果一定要训练网络的主干部分，可以了解imagenet数据集，首先训练分类模型，分类模型的 主干部分 和该模型通用，基于此进行训练。
+    #----------------------------------------------------------------------------------------------------------------------------#
+    model_path      = 'model_data/voc_weights_resnet.h5'
+    #------------------------------------------------------#
+    #   输入的shape大小
+    #------------------------------------------------------#
+    input_shape     = [600, 600]
+    #---------------------------------------------#
+    #   vgg或者resnet50
+    #---------------------------------------------#
+    backbone        = "resnet50"
+    #------------------------------------------------------------------------#
+    #   anchors_size用于设定先验框的大小，每个特征点均存在9个先验框。
+    #   anchors_size每个数对应3个先验框。
+    #   当anchors_size = [8, 16, 32]的时候，生成的先验框宽高约为：
+    #   [90, 180] ; [180, 360]; [360, 720]; [128, 128]; 
+    #   [256, 256]; [512, 512]; [180, 90] ; [360, 180]; 
+    #   [720, 360]; 详情查看anchors.py
+    #   如果想要检测小物体，可以减小anchors_size靠前的数。
+    #   比如设置anchors_size = [64, 256, 512]
+    #------------------------------------------------------------------------#
+    anchors_size    = [128, 256, 512]
+
+    #----------------------------------------------------#
+    #   训练分为两个阶段，分别是冻结阶段和解冻阶段。
+    #   显存不足与数据集大小无关，提示显存不足请调小batch_size。
+    #----------------------------------------------------#
+    #----------------------------------------------------#
+    #   冻结阶段训练参数
+    #   此时模型的主干被冻结了，特征提取网络不发生改变
+    #   占用的显存较小，仅对网络进行微调
+    #----------------------------------------------------#
+    Init_Epoch          = 0
+    Freeze_Epoch        = 50
+    Freeze_batch_size   = 4
+    Freeze_lr           = 1e-4
+    #----------------------------------------------------#
+    #   解冻阶段训练参数
+    #   此时模型的主干不被冻结了，特征提取网络会发生改变
+    #   占用的显存较大，网络所有的参数都会发生改变
+    #----------------------------------------------------#
+    UnFreeze_Epoch      = 100
+    Unfreeze_batch_size = 2
+    Unfreeze_lr         = 1e-5
+    #------------------------------------------------------#
+    #   是否进行冻结训练，默认先冻结主干训练后解冻训练。
+    #------------------------------------------------------#
+    Freeze_Train        = True
+    #----------------------------------------------------#
+    #   获得图片路径和标签
+    #----------------------------------------------------#
+    train_annotation_path   = '2007_train.txt'
+    val_annotation_path     = '2007_val.txt'
+
+    #----------------------------------------------------#
+    #   获取classes和anchor
+    #----------------------------------------------------#
+    class_names, num_classes = get_classes(classes_path)
+    num_classes += 1
+    anchors = get_anchors(input_shape, backbone, anchors_size)
+
+    K.clear_session()
+    model_rpn, model_all = get_model(num_classes, backbone = backbone)
+    if model_path != '':
+        #------------------------------------------------------#
+        #   载入预训练权重
+        #------------------------------------------------------#
+        print('Load weights {}.'.format(model_path))
+        model_rpn.load_weights(model_path, by_name=True)
+        model_all.load_weights(model_path, by_name=True)
+
+    #--------------------------------------------#
+    #   训练参数的设置
+    #--------------------------------------------#
+    callback        = tf.summary.create_file_writer("logs")
+    loss_history    = LossHistory("logs/")
+
+    bbox_util       = BBoxUtility(num_classes)
+    roi_helper      = ProposalTargetCreator(num_classes)
+    #---------------------------#
+    #   读取数据集对应的txt
+    #---------------------------#
+    with open(train_annotation_path) as f:
+        train_lines = f.readlines()
+    with open(val_annotation_path) as f:
+        val_lines   = f.readlines()
+    num_train   = len(train_lines)
+    num_val     = len(val_lines)
+
+    freeze_layers = {'vgg' : 17, 'resnet50' : 141}[backbone]
+    if Freeze_Train:
+        for i in range(freeze_layers): 
+            if type(model_all.layers[i]) != tf.keras.layers.BatchNormalization:
+                model_all.layers[i].trainable = False
+        print('Freeze the first {} layers of total {} layers.'.format(freeze_layers, len(model_all.layers)))
+
+    #------------------------------------------------------#
+    #   主干特征提取网络特征通用，冻结训练可以加快训练速度
+    #   也可以在训练初期防止权值被破坏。
+    #   Init_Epoch为起始世代
+    #   Freeze_Epoch为冻结训练的世代
+    #   Unfreeze_Epoch总训练世代
+    #   提示OOM或者显存不足请调小Batch_size
+    #------------------------------------------------------#
+    if True:
+        batch_size  = Freeze_batch_size
+        lr          = Freeze_lr
+        start_epoch = Init_Epoch
+        end_epoch   = Freeze_Epoch
+
+        epoch_step      = num_train // batch_size
+        epoch_step_val  = num_val // batch_size
+
+        if epoch_step == 0 or epoch_step_val == 0:
+            raise ValueError('数据集过小，无法进行训练，请扩充数据集。')
+
+        model_rpn.compile(
+            loss = {
+                'classification': rpn_cls_loss(),
+                'regression'    : rpn_smooth_l1()
+            }, optimizer = Adam(lr=lr)
+        )
+        model_all.compile(
+            loss = {
+                'classification'                        : rpn_cls_loss(),
+                'regression'                            : rpn_smooth_l1(),
+                'dense_class_{}'.format(num_classes)    : classifier_cls_loss(),
+                'dense_regress_{}'.format(num_classes)  : classifier_smooth_l1(num_classes - 1)
+            }, optimizer = Adam(lr=lr)
+        )
+
+        gen     = FRCNNDatasets(train_lines, input_shape, anchors, batch_size, num_classes, train = True).generate()
+        gen_val = FRCNNDatasets(val_lines, input_shape, anchors, batch_size, num_classes, train = False).generate()
+
+        print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
+        for epoch in range(start_epoch, end_epoch):
+            fit_one_epoch(model_rpn, model_all, loss_history, callback, epoch, epoch_step, epoch_step_val, gen, gen_val, end_epoch,
+                    anchors, bbox_util, roi_helper)
+            lr = lr*0.96
+            K.set_value(model_rpn.optimizer.lr, lr)
+            K.set_value(model_all.optimizer.lr, lr)
+
+    if Freeze_Train:
+        for i in range(freeze_layers): 
+            if type(model_all.layers[i]) != tf.keras.layers.BatchNormalization:
+                model_all.layers[i].trainable = True
+
+    if True:
+        batch_size  = Unfreeze_batch_size
+        lr          = Unfreeze_lr
+        start_epoch = Freeze_Epoch
+        end_epoch   = UnFreeze_Epoch
+
+        epoch_step      = num_train // batch_size
+        epoch_step_val  = num_val // batch_size
+
+        if epoch_step == 0 or epoch_step_val == 0:
+            raise ValueError('数据集过小，无法进行训练，请扩充数据集。')
+
+        model_rpn.compile(
+            loss = {
+                'classification': rpn_cls_loss(),
+                'regression'    : rpn_smooth_l1()
+            }, optimizer = Adam(lr=lr)
+        )
+        model_all.compile(
+            loss = {
+                'classification'                        : rpn_cls_loss(),
+                'regression'                            : rpn_smooth_l1(),
+                'dense_class_{}'.format(num_classes)    : classifier_cls_loss(),
+                'dense_regress_{}'.format(num_classes)  : classifier_smooth_l1(num_classes - 1)
+            }, optimizer = Adam(lr=lr)
+        )
+
+        gen     = FRCNNDatasets(train_lines, input_shape, anchors, batch_size, num_classes, train = True).generate()
+        gen_val = FRCNNDatasets(val_lines, input_shape, anchors, batch_size, num_classes, train = False).generate()
+
+        print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
+        for epoch in range(start_epoch, end_epoch):
+            fit_one_epoch(model_rpn, model_all, loss_history, callback, epoch, epoch_step, epoch_step_val, gen, gen_val, end_epoch,
+                    anchors, bbox_util, roi_helper)
+            lr = lr*0.96
+            K.set_value(model_rpn.optimizer.lr, lr)
+            K.set_value(model_all.optimizer.lr, lr)
